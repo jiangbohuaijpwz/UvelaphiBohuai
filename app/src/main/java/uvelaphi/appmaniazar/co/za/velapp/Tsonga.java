79150c75896e7de40e3926fd8645ed7055f15e04
siyaMkosi <cece.marela@gmail.com>
2018-07-09 13:15:49 +0200

    first commit

---
+++ app/src/main/java/uvelaphi/appmaniazar/co/za/velapp/Tsonga.java
@@ -0,0 +1,28 @@
+package uvelaphi.appmaniazar.co.za.velapp;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by siyanda on 11/12/17.
+ */
+
+public class Tsonga extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.tsonga ,container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        getActivity().setTitle("Tsonga");
+    }
+}
